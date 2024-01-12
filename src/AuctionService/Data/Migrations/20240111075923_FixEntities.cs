22040592ceaff70d41e39b1cff4374f4e45cfd57
Nguyen Dinh Hoang Huy <0788909221ndhh@gmail.com>
2024-01-12 10:12:02 +0700

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20240111075923_FixEntities.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SOldAmount",
+                table: "Auctions",
+                newName: "SoldAmount");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrenntHighBid",
+                table: "Auctions",
+                newName: "CurrentHighBid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SoldAmount",
+                table: "Auctions",
+                newName: "SOldAmount");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentHighBid",
+                table: "Auctions",
+                newName: "CurrenntHighBid");
+        }
+    }
+}
