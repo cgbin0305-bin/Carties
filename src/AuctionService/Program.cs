f9d5b4d39ebdc7ff04e30f2e1c055de97113d15a
Nguyen Dinh Hoang Huy <0788909221ndhh@gmail.com>
2024-01-23 10:19:26 +0700

    End of section 4

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using AuctionService;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,32 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
   opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+builder.Services.AddMassTransit(x =>
+{
+  x.AddEntityFrameworkOutbox<AuctionDbContext>(opt =>
+  {
+    /*
+    each 10 second it will check the outbox 
+    
+    if the service is available => the message will be delivered  
+    
+    if it's is not => every 10s, it's going to attemp inside our outbox and see if anything hasn't been delivered yet
+    */
+
+    opt.QueryDelay = TimeSpan.FromSeconds(20);
+    opt.UsePostgres();
+    opt.UseBusOutbox();
+  });
+  //make the connection to rabbitMQ (use local host) 
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+  x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    cfg.ConfigureEndpoints(context);
+  });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
@@ -7,6 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+  x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    // receive the publish 
+    cfg.ReceiveEndpoint("search-auction-created", e =>
+    {
+      // retry it when something went wrong in 5 times each times is 5 second
+      e.UseMessageRetry(r => r.Interval(5, 15));
+
+      e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+    });
+    cfg.ConfigureEndpoints(context);
+  });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
